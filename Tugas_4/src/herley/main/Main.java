0eecd99855919fa56a5766953efe2a4e9eb9bcb6
herley-shaori <33199364+herley-shaori@users.noreply.github.com>
2017-11-11 03:08:10 +0700

    Add files via upload

--- Tugas_4/src/herley/main/Main.java
+++ Tugas_4/src/herley/main/Main.java
@@ -13,9 +13,6 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Aku Sayang Dirimu");
-        System.out.println();
-
         try{
             final InputStreamReader reader = new InputStreamReader(System.in);
             final BufferedReader jumlah = new BufferedReader(reader);
